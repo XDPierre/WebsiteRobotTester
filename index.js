8b21cc501735833322f62fada6cfb5e75d0fa0c9
XDPierre <43551834+XDPierre@users.noreply.github.com>
2023-06-15 22:45:28 -0300

    Changing the main code, so it waits for the site do load
    Making the page wait for the page to load

--- index.js
+++ index.js
@@ -10,10 +10,44 @@
 //   await container.stop();
 // }
 
+const axios = require('axios');
+const fetch = import('node-fetch').default;
+
+async function waitForGrid() {
+  const gridUrl = 'http://selenium-hub:4444/wd/hub/status';
+  const cmd = process.argv.slice(2).join(' ');
+
+  while (true) {
+    try {
+      const response = await axios.get(gridUrl);
+      const jsonData = await response.data;
+      const isGridReady = jsonData.value.ready;
+
+      if (isGridReady === true) {
+        console.error('Selenium Grid is up - executing tests');
+        exec(cmd);
+        break;
+      } else {
+        console.log('Waiting for the Grid');
+        await sleep(1000);
+      }
+    } catch (error) {
+      console.error('Error occurred while checking Grid status:', error.message);
+      await sleep(1000);
+    }
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const chrome = require("selenium-webdriver/chrome");
 const { Builder, By, Key, until } = require("selenium-webdriver");
 
-async function main() {
+async function exec(cmd) {
+  // Implement your logic to execute the tests here
+  console.log('Executing tests:', cmd);
 
   let driver = await new Builder()
   .forBrowser("chrome")
@@ -27,25 +61,39 @@ async function main() {
 
     // Click
     let button = driver.findElement(By.css('body > div > section.section-3.wf-section > div > div.w-layout-grid.grid-8 > a:nth-child(3) > div > div.text-block-20'));
-    button.click();
-  
-    // Get the active tabs
-    let windowHandles = await driver.getWindowHandles();
-    let tabs = Array.from(windowHandles);
-  
-    // Enter waiting list
-    await driver.switchTo().window(tabs.get(1));
-  
-    let firstResult = await driver.wait(
-      until.elementLocated(By.css("body > #MainPart_lbUsersInLineAheadOfYou")),  
-      10000
-    );
+   
+    let tabs = [];
+
+    do {
+      button.click();
+      // Get the active tabs
+      let windowHandles = await driver.getAllWindowHandles();
+      tabs = Array.from(windowHandles);
+      console.log('Insufficient tabs available.');
+    } while (tabs.length < 2);
+    
+    await driver.switchTo().window(tabs[1].toString());
+
+    let firstResult;
+
+    do {
+      try {
+        firstResult = await driver.wait(
+          until.elementLocated(By.css("body > #MainPart_lbUsersInLineAheadOfYou")),
+          1000
+        );
+      } catch (error) {
+        // Handle any errors or timeouts here
+        console.log('Element not found yet. Retrying...');
+      }
+    } while (!firstResult);
+    
     console.log('Posicao Fila:');
     console.log(parseInt(firstResult.getAttribute("textContent")));
     // Shut down the container if the condition is met
     if (parseInt(firstResult.getAttribute("textContent")) > 100000) {
       // stopContainer('taylorshow1');
-      console.log('NADA AQUI');
+      console.log('---------------NADA AQUI--------------');
     }
   
   } finally {
@@ -53,4 +101,4 @@ async function main() {
   }
 }
 
-main().catch(console.error);
+waitForGrid();
